7d614955d89dca7426c163a956d66d32b1e37225
manshyworld <101945110+manshyworld@users.noreply.github.com>
2023-06-04 17:53:19 +0300

    rename_series_keep_s0e0.py

--- rename_series_keep_s0e0.py
+++ rename_series_keep_s0e0.py
@@ -2,6 +2,22 @@ import os
 import re
 import sys
 
+def rename_files(directory, series_name, pattern):
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            file_name, file_ext = os.path.splitext(filename)
+            match = re.search(pattern, filename, re.IGNORECASE)
+
+            if match:
+                season_number = match.group(1)
+                episode_number = match.group(2)
+                
+                new_file_name = f"{series_name} S{season_number}E{episode_number}{file_ext}"
+                current_path = os.path.join(root, filename)
+                new_path = os.path.join(root, new_file_name)
+                os.rename(current_path, new_path)
+                print(f"Renamed {filename} to {new_file_name}.")
+
 directory = input("Enter the directory path: ")
 series_name = input("Enter the series name: ")
 pattern = r"s(\d{2})e(\d{2})"
@@ -10,14 +26,4 @@ if not os.path.isdir(directory):
     print("Invalid directory path. Please try again.")
     sys.exit(1)
 
-for filename in os.listdir(directory):
-    file_name, file_ext = os.path.splitext(filename)
-    match = re.search(pattern, filename, re.IGNORECASE)
-
-    if match:
-        season_number = match.group(1)
-        episode_number = match.group(2)
-    
-        new_file_name = f"{series_name} S{season_number}E{episode_number}{file_ext}"
-        os.rename(os.path.join(directory, filename), os.path.join(directory, new_file_name))
-        print(f"Renamed {file_name} to {new_file_name}.")
+rename_files(directory, series_name, pattern)
